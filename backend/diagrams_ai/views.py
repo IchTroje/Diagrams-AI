ff79ea9e2a61814bb1b198be29e29f052a4bd390
Korbielowski <dawidstasinski12@gmail.com>
2025-04-13 13:39:27 +0200

    Add static and templates, format all python files

--- backend/diagrams_ai/views.py
+++ backend/diagrams_ai/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(req):
-    return  HttpResponse("<h1>Hello here :)</h1>")
+
+def index(request):
+    return render(request, "diagrams_ai/base.html")
