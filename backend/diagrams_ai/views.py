5f2fd0939b6d4cfd9ec0344fef437caa590a4794
Korbielowski <dawidstasinski12@gmail.com>
2025-04-21 23:51:38 +0200

    Add function for new endpoint, for now it returns the data that it gets

--- backend/diagrams_ai/views.py
+++ backend/diagrams_ai/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
 
 def index(request):
     return render(request, "diagrams_ai/base.html")
+
+
+def send_prompt_to_llm(request):
+    if request.method == "POST":
+        prompt = json.loads(request.body.decode("utf-8"))["body"]
+        return JsonResponse({"message": f"{prompt}"}, safe=False)
+    return JsonResponse({"message": "Wszystko jest git :)"}, safe=False)
