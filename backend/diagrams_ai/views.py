89c940713e2a95997c2ad1c2346e7c18214e3637
Korbielowski <dawidstasinski12@gmail.com>
2025-04-23 07:06:35 +0200

    Add connection with LLM via API

--- backend/diagrams_ai/views.py
+++ backend/diagrams_ai/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import os
+from openai import OpenAI
+
+LLM_CLIENT = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
 
 
 def index(request):
@@ -9,8 +16,27 @@ def index(request):
 
 def send_prompt_to_llm(request):
     if request.method == "POST":
-        prompt = json.loads(request.body.decode("utf-8"))["body"]
-        return JsonResponse({"message": f"{prompt}"}, safe=False)
+        try:
+            data = json.loads(request.body.decode("utf-8"))
+            prompt = data.get("body", "").strip()
+
+            if not prompt:
+                return JsonResponse(
+                    {"message": "Something went wrong with getting prompt to server"},
+                    status=400,
+                )
+
+            completion = LLM_CLIENT.chat.completions.create(
+                extra_body={},
+                model="nvidia/llama-3.1-nemotron-ultra-253b-v1:free",
+                messages=[{"role": "user", "content": prompt}],
+            )
+            return JsonResponse(
+                {"message": f"{completion.choices[0].message.content}"}, safe=False
+            )
+        except json.JSONDecodeError:
+            return JsonResponse({"message": "Wrong JSON format"}, status=400)
+
     return JsonResponse(
         {"message": "Other method HTTP method used, only POST works for this endpoint"},
         safe=False,
